0bd348be4f017d319f5f0a398a27ddd3ca850682
niftypliers <niftypliers@gmail.com>
2023-04-06 06:59:58 +0530

    ES6 - Day 1 - Usage of var let and const

---
+++ js/es6.js
@@ -0,0 +1,14 @@
+
+const person = {
+    'name': 'bala',
+    'age': 30,
+    'eat': function (a, b) {
+        return a + b;
+    }
+};
+
+console.log(person);
+
+console.log(person.name);
+
+console.log(person.eat(5,3));
