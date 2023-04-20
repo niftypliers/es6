5f908b82b5830845a6f0a123a9408dba7814cf3b
niftypliers <niftypliers@gmail.com>
2023-04-20 07:05:04 +0530

    ES6 - Day 11 - Callback and Promises

--- js/es6.js
+++ js/es6.js
@@ -1,67 +1,27 @@
-// Iterators.
-let arr = [1, 2, 3];
-let it_arr = arr[Symbol.iterator]();
-console.log(arr);
-console.log(it_arr.next());
-console.log(it_arr.next());
-console.log(it_arr.next());
-console.log(it_arr.next());
+// Async
 
-let obj = {
-  fName: "Ram",
-  age: 30
-};
-
-obj[Symbol.iterator] = function() {
-  return {
-    next: () => {
-      return {
-        done: false,
-        value: "hi"
-      }
-    }
-  };
-}
-
-console.log(obj);
-let it_obj = obj[Symbol.iterator]();
-console.log(it_obj.next());
+setTimeout(function() {
+  console.log(3000);
+}, 3000);
 
 
-// Generators
-function *nameGenerator() {
-  yield 'Ram';
-  yield 'Sara';
-}
+// Promise
 
-function *stringGenerator() {
-  yield *nameGenerator();
-  yield 'hi';
-  yield 'yes';
-}
+let fileReadStatus = new Promise(function(resolve, reject) {
+  // Read  a file.
+  let file = false;
+  if (file) {
+    resolve("Done");
+  } else {
+    reject("Some error");
+  }
+});
 
-const strings = stringGenerator();
-
-console.log(strings.next());
-console.log(strings.next());
-console.log(strings.next());
-console.log(strings.next());
-
-
-// Passing value to Generators.
-function *overrideValue() {
-  const result = yield 'hi';
-  console.log("gen:" + result);
-}
-
-const overrideIterator = overrideValue();
-console.log(overrideIterator.next());
-console.log(overrideIterator.next('abc'));
-
-
-/*
-REF:
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
-https://blog.logrocket.com/javascript-iterators-and-generators-a-complete-guide/
-https://madasamy.medium.com/explanation-about-iterators-and-generators-in-javascript-es6-f7e669cbe96e
-*/
+fileReadStatus.then(
+  function (value) {
+    console.log(value);
+  },
+  function (error) {
+    console.log(error)
+  }
+);
